44a8c378b8d147e0c7d7e5960c1e357268a58bea
Teddie Finström <teddie.finstrom@outlook.com>
2025-06-03 13:36:56 +0200

    Fixed responsitivity

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,18 +3,16 @@ import Card from "react-bootstrap/Card";
 const Projects = () => {
   return (
     <>
-      <div className="container-content">
+      <div className="portfolio-content">
         <div className='portfolio-cards'>
-        <Card style={{ maxWidth: '18rem' }}>
+        <Card className="card">
           <Card.Body>
             <Card.Title>Guess The Student</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">
               Made with: HTML, CSS, Javascript and Bootstrap
             </Card.Subtitle>
             <Card.Text>
-              A JavaScript-based quiz game where players can choose the number
-              of questions and try to guess which classmate is shown in each
-              photo.
+            A JavaScript-based quiz game where players can select how many questions they want to answer and try to guess which classmate is shown in each photo. The game keeps track of the score and provides instant feedback after each guess.
             </Card.Text>
             <Card.Link href="https://github.com/the-hive-resistance/fed24-js1-uppgift-1-Teddiefinstrom.git">Github</Card.Link>
             <Card.Link href="https://sensational-semifreddo-02a02f.netlify.app/">
@@ -23,15 +21,14 @@ const Projects = () => {
           </Card.Body>
         </Card>
 
-        <Card style={{ maxWidth: '18rem' }}>
+        <Card className="card">
           <Card.Body>
             <Card.Title>Bortakväll</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">
               Made with: HTML, CSS, TypeScript, Vite, Bootstrap
             </Card.Subtitle>
             <Card.Text>
-            A group project for an online candy store where we fetched product data from an external API. Users can browse candy, add items to a shopping cart, and complete their purchase by filling out an order form. I was mainly responsible for developing the form functionality, ensuring users could enter their information and receive an order confirmation number.
-            </Card.Text>
+            A group project for an online candy store using data from an external API. Users can browse products, add items to a cart, and complete purchases via an order form. I developed the form functionality to handle user info and order confirmations.            </Card.Text>
             <Card.Link href="https://github.com/the-hive-resistance/fed24-js1-uppgift-2-fed24-js1-inlamningsuppgift-2-grupp-9.git">Github</Card.Link>
             <Card.Link href="https://bortakvallgrupp9.netlify.app/">
               Live Demo
@@ -39,7 +36,7 @@ const Projects = () => {
           </Card.Body>
         </Card>
 
-        <Card style={{ maxWidth: '18rem' }}>
+        <Card className="card">
           <Card.Body>
             <Card.Title>Star Wars Encyclopedia</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">
