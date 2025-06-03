eae5aaf1b2e3752233edee57d94908b90af83196
Teddie Finström <teddie.finstrom@outlook.com>
2025-06-03 14:31:44 +0200

    changes route

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/teddie-portfolio">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
