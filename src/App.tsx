277aac4cebdbe4f41577715221eeb3929ebc3c2a
Teddie Finström <teddie.finstrom@outlook.com>
2025-06-03 14:03:02 +0200

    depolying

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import './assets/scss/App.scss'
-import { Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
 import Navbar from './component/Navbar';
 
-
 function App() {
-
   return (
-    <>
-<div className='background-container'>
-<Navbar />
-<Routes>
-  <Route path="/" element={<HomePage />} />
-  <Route path="/contact" element={<Contact />} />
-  <Route path="/projects" element={<Projects />} />
-</Routes>
-</div>
-    </>
+    <BrowserRouter basename="/teddie-portfolio">
+      <div className='background-container'>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
